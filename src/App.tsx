28e68a5098f3dc22ba57b668c6217dfdc56e76f5
宮脇隆志 <onsinhutuu@gmail.com>
2020-12-19 00:22:32 +0900

    Finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import React, {useState, useEffect} from "react";
 import { FormControl, List, TextField } from "@material-ui/core";
 import AddToPhotosIcon from "@material-ui/icons/AddToPhotos";
-import './App.css';
-import {db} from "./firebase";
-const App: React.FC = () => {
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import styles from "./App.module.css";
+import { makeStyles } from "@material-ui/styles";
+import {db, auth} from "./firebase";
+import { TaskItem } from "./components/TaskItem";
+const useStyles = makeStyles({
+  field: {
+    marginTop: 30,
+    marginBottom: 20,
+  },
+  list: {
+    margin: "auto",
+    width: "40%",
+  },
+});
+
+const App: React.FC = (props: any) => {
   const [tasks, setTasks] = useState([{id:"", title:""}]);
   const [input, setInput] = useState("");
+  const classes = useStyles();
+
+  useEffect(() => {
+    const unSub = auth.onAuthStateChanged((user) => {
+      !user && props.history.push("login");
+    });
+    return () => unSub();
+  });
+
   useEffect(() => {
     const unSub = db.collection("tasks").onSnapshot((snapshot)=> {
       setTasks(
@@ -23,19 +46,40 @@ const App: React.FC = () => {
     setInput("");
   }
   return (
-    <div className="App">
+    <div className={styles.app__root}>
       <h1>Todo App by React/firebase</h1>
+      <button
+        className={styles.app__logout}
+        onClick={async () => {
+          try {
+            await auth.signOut();
+            props.history.push("login");
+          } catch (error) {
+            alert(error.message);
+          }
+        }}
+      >
+        <ExitToAppIcon />
+      </button>
+      <br />
        <FormControl>
          <TextField 
+         className={classes.field}
+         InputLabelProps={{
+          shrink: true,
+        }}
          label="New task ?"
          value={input}
          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInput(e.target.value)}
          />
        </FormControl>
-        <button disabled={!input} onClick={newTask}>
+        <button className={styles.app__icon} disabled={!input} onClick={newTask}>
           <AddToPhotosIcon />
         </button>
-      {tasks.map((task) => (<h3 key={task.id}>{task.title}</h3>))}
+        <List className={classes.list}>
+        {tasks.map((task) => (<TaskItem key={task.id} id={task.id} title={task.title}/>))}
+        </List>
+      
     </div>
   );
 }
